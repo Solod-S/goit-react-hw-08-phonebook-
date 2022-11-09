0254bdf7c9ec28d4e405a2fd7ddd4c637cfd4285
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-11-09 17:18:14 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { AppBar } from './AppBar/AppBar';
 import AuthenticationPage from 'pages/Authentication/Authentication';
 import ContactPage from 'pages/ContactPage/ContactPage';
-import HomePage from 'pages/HomePage/Homepage';
+// import HomePage from 'pages/HomePage/Homepage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route end path="/" element={<AppBar />}>
-          <Route index element={<HomePage />} />
-          {/* <Route index element={<div>Home</div>} /> */}
+          {/* <Route index element={<HomePage />} /> */}
+          <Route index element={<div>Home</div>} />
           <Route path="authentication" element={<AuthenticationPage />} />
           <Route path="contacts" element={<ContactPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
