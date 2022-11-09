4288e346146ad67197d1d703c90465dee819ccfc
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-11-09 12:59:05 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,14 @@ import { AppBar } from './AppBar/AppBar';
 import AuthenticationPage from 'pages/Authentication/Authentication';
 import ContactPage from 'pages/ContactPage/ContactPage';
 // import HomePage from 'pages/HomePage/Homepage';
-import HomePage from 'pages/HomePage/Homepage';
+// import HomePage from 'pages/HomePage/Homepage';
 function App() {
   return (
     <>
       <Routes>
         <Route end path="/" element={<AppBar />}>
-          <Route index element={<HomePage />} />
+          {/* <Route index element={<HomePage />} /> */}
+          <Route index element={<div>Home</div>} />
           <Route path="authentication" element={<AuthenticationPage />} />
           <Route path="contacts" element={<ContactPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
