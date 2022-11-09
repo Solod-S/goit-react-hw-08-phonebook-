85f219500f887beee1da8f8cdd80b5c6507e0f5b
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-11-09 12:55:28 +0200

    +

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,25 +1,13 @@
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from 'react';
-import {
-  AppHeader,
-  MainAppHeader,
-  Section,
-  ImgLeft,
-  ImgRigth,
-} from './Authentication.sttyled';
+import { Section, ImgLeft, ImgRigth } from './Authentication.sttyled';
 import { Registration } from 'components/Registration/Registration';
 import { LogIn } from 'components/LogIn/LogIn';
-// import ContactForm from 'components/ContactForm/ContactForm';
-// import ContactList from 'components/ContactList/ContactList';
-// import { EditContactModal } from 'components/EditContactModal/EditContactModal';
-// import Filter from 'components/Filter/Filter';
 import image from '../../img/left.png';
 import image2 from '../../img/right.png';
 import { Box } from 'components/Box/Box';
-import { useSelector } from 'react-redux';
 
 const AuthenticationPage = () => {
-  const modalIsActive = useSelector(state => state.modal.isActive);
   const [logInToShown, setLogInToShown] = useState(true);
   return (
     <Box display="flex" justifyContent="center">
@@ -31,13 +19,6 @@ const AuthenticationPage = () => {
         ) : (
           <Registration setLogInToShown={setLogInToShown} />
         )}
-
-        {/* {modalIsActive && <EditContactModal />}
-        <MainAppHeader>Phonebook</MainAppHeader>
-        <ContactForm />
-        <AppHeader>Contacts</AppHeader>
-        <Filter />
-        <ContactList /> */}
       </Section>
     </Box>
   );
